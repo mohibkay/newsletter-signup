3434660d5cd9ecfb474db963ae48fbc30b7efefd
mohibkay <mohibk.in@gmail.com>
2023-07-15 22:45:34 +0530

    fix: added robust email regex

--- js/main.js
+++ js/main.js
@@ -35,7 +35,8 @@ function setSuccessFor(input) {
 }
 
 function isEmailValid(email) {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailRegex =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return emailRegex.test(email);
 }
 
